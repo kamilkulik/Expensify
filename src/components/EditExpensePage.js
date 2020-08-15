7582e45a926cabc5d4619039162263c170f12398
Kamil <37276273+kamilkulik@users.noreply.github.com>
2020-08-15 11:38:35 +0200

    update styling

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -7,41 +7,38 @@ export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
     this.props.startEditExpense(this.props.expense.id, expense);
     this.props.history.push('/');
-  }
-  removeExpense = () => {
+  };
+  onRemove = () => {
     this.props.startRemoveExpense({ id: this.props.expense.id });
     this.props.history.push('/');
-  }
+  };
   render() {
     return (
       <div>
-        Editing the expense with id of {this.props.expense.id}:
-        <ExpenseForm
-          expense={this.props.expense}
-          onSubmit={this.onSubmit}
-        />
-        <button 
-          onClick={this.removeExpense}
-        >
-        Remove
-        </button>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Edit Expense</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm
+            expense={this.props.expense}
+            onSubmit={this.onSubmit}
+          />
+          <button className="button button--secondary" onClick={this.onRemove}>Remove Expense</button>
+        </div>
       </div>
     );
   }
-  
 };
 
-const mapDispatchToProps = (dispatch, props) => {
-  return {
-    startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
-    startRemoveExpense: (data) => dispatch(startRemoveExpense( data )),
-  }
-};
+const mapStateToProps = (state, props) => ({
+  expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+});
 
-const mapStateToProps = (state, props) => {
-  return {
-    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
-  }
-};
+const mapDispatchToProps = (dispatch, props) => ({
+  startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
+  startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -15,16 +15,19 @@ export default class ExpenseForm extends React.Component {
       error: ''
     };
   }
-  onChange = (e) => {
-    const target = e.target;
-    const name = target.name;
-    const value = target.value;
-    this.setState({ [name]: value });
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+  };
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }));
   };
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
-      this.setState({ amount });
+
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+      this.setState(() => ({ amount }));
     }
   };
   onDateChange = (createdAt) => {
@@ -33,60 +36,61 @@ export default class ExpenseForm extends React.Component {
     }
   };
   onFocusChange = ({ focused }) => {
-    this.setState({ calendarFocused: focused })
+    this.setState(() => ({ calendarFocused: focused }));
   };
   onSubmit = (e) => {
     e.preventDefault();
 
     if (!this.state.description || !this.state.amount) {
-      this.setState({ error: 'Add description and amount' })
+      this.setState(() => ({ error: 'Please provide description and amount.' }));
     } else {
-      this.setState({ error: '' })
-      this.props.onSubmit({ 
+      this.setState(() => ({ error: '' }));
+      this.props.onSubmit({
         description: this.state.description,
         amount: parseFloat(this.state.amount, 10) * 100,
         createdAt: this.state.createdAt.valueOf(),
         note: this.state.note
-       });
+      });
     }
   };
   render() {
     return (
-      <div>
-        {this.state.error && <p style={{color: "red"}}>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
-          <input 
-            name="description"
-            type="text"
-            placeholder="Description"
-            autoFocus
-            value={this.state.description}
-            onChange={this.onChange}
-          />
-          <input 
-            type="text"
-            placeholder="Amount"
-            value={this.state.amount}
-            onChange={this.onAmountChange}
-          />
-          <SingleDatePicker 
-            date={this.state.createdAt}
-            onDateChange={this.onDateChange}
-            focused={this.state.calendarFocused}
-            onFocusChange={this.onFocusChange}
-            numberOfMonths={1}
-            isOutsideRange={() => false}
-          />
-          <textarea
-            name="note"
-            placeholder="Add a note for your expense(optional)"
-            value={this.state.note}
-            onChange={this.onChange}
-          >
-          </textarea>
-          <button>Add Expense</button>
-        </form>
-      </div>
+      <form className="form" onSubmit={this.onSubmit}>
+        {this.state.error && <p className="form__error">{this.state.error}</p>}
+        <input
+          type="text"
+          placeholder="Description"
+          autoFocus
+          className="text-input"
+          value={this.state.description}
+          onChange={this.onDescriptionChange}
+        />
+        <input
+          type="text"
+          placeholder="Amount"
+          className="text-input"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        />
+        <SingleDatePicker
+          date={this.state.createdAt}
+          onDateChange={this.onDateChange}
+          focused={this.state.calendarFocused}
+          onFocusChange={this.onFocusChange}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+        <textarea
+          placeholder="Add a note for your expense (optional)"
+          className="textarea"
+          value={this.state.note}
+          onChange={this.onNoteChange}
+        >
+        </textarea>
+        <div>
+          <button className="button">Save Expense</button>
+        </div>
+      </form>
     )
   }
 }
